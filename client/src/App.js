62dc70ff484b13ae5a275da360c242edb9fd82ab
Engr.Sunday <asmelitus@gmail.com>
2018-08-08 23:27:06 +0100

    added eventSource api to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,11 +24,41 @@ class App extends Component {
       Header: 'State',
       accessor: 'state'
     }];
+
+    this.eventSource = new EventSource('http://localhost:5000/events');
+  }
+
+  componentDidMount() {
+    this.eventSource.addEventListener('flightStateUpdate', (e) => this.updateFlightState(JSON.parse(e.data)));
+    this.eventSource.addEventListener('flightRemoval', (e) => this.removeFlight(JSON.parse(e.data)));
+    this.eventSource.addEventListener('closedConnection', () => this.stopUpdates());
+  }
+
+  updateFlightState(flightState) {
+    let newData = this.state.data.map((item) => {
+      if (item.flight === flightState.flight) {
+        item.state = flightState.state;
+      }
+      return item;
+    });
+
+    this.setState(Object.assign({}, {data: newData}));
+  }
+
+  removeFlight(flightInfo) {
+    const newData = this.state.data.filter((item) => item.flight !== flightInfo.flight);
+
+    this.setState(Object.assign({}, {data: newData}));
+  }
+
+  stopUpdates() {
+    this.eventSource.close();
   }
 
   render() {
     return (
       <div className="App">
+        <button onClick={() => this.stopUpdates()} style={{backgroundColor:"green"}}>Stop updates</button>
         <ReactTable
           data={this.state.data}
           columns={this.columns}
